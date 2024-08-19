729fd63b97cdd7b61fe5a246ef94cb1b91897f19
Rixbroho <ratnaprajapati10@gmail.com>
2024-08-19 18:32:09 +0545

    Making Nav and slider, making project section but not photo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // window.onscroll=function(){headerShadow()};
-AOS.init({
-    offset:280,
-    duration:800,
- });
+// AOS.init({
+//     offset:280,
+//     duration:800,
+//  });
 
 
-const a=document.querySelector('.menu-option');
 function menu(){
-    a.classList.toggle('off');
+    console.log(2);
+    document.querySelector('.menu-option').classList.toggle('off');
     document.querySelector('body').classList.toggle('stop');
 }
