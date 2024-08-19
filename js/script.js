666c9dd96b92f3d5fc57a76fa4d3747c0f2abf03
Rixbroho <ratnaprajapati10@gmail.com>
2024-08-19 19:27:54 +0545

    no thing change

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 
 function menu(){
     console.log(2);
+    // document.querySelector('.menu-option').classList.toggle('on');
     document.querySelector('.menu-option').classList.toggle('off');
     document.querySelector('body').classList.toggle('stop');
 }
